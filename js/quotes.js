ac089ce405bfa8f920b1a8eb92de4f1d9d87592d
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-03-31 11:38:00 +0700

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -30,39 +30,39 @@ quotes[12]='<p>Man cannot discover new oceans unless he has the courage to lose 
 
 quotes[13]='<p>The wise man does at once what the fool does finally.<p> <p><cite>-Baltasar Gracian</cite></p>'
 
-quotes[14]='<p>The world has the habit of making room for the man whose actions show that he knows where he is going. <i>-Napoleon Hill</i>'
+quotes[14]='<p>The world has the habit of making room for the man whose actions show that he knows where he is going.<p> <p><cite>-Napoleon Hill</cite></p>'
 
-quotes[15]='<p>Success seems to be connected with action. Successful men keep moving. They make mistakes, but they don\'t quit. <i>-Conrad Hilton</i>'
+quotes[15]='<p>Success seems to be connected with action. Successful men keep moving. They make mistakes, but they don\'t quit.<p> <p><cite>-Conrad Hilton</cite></p>'
 
-quotes[16]='<p>Do the things you know, and you shall learn the truth you need to know. <i>-George Macdonald</i>'
+quotes[16]='<p>Do the things you know, and you shall learn the truth you need to know.<p> <p><cite>-George Macdonald</cite></p>'
 
-quotes[17]='<p>I have never heard anything about the resolutions of the apostles, but a good deal about their acts. <i>-Horace Mann</i>'
+quotes[17]='<p>I have never heard anything about the resolutions of the apostles, but a good deal about their acts.<p> <p><cite>-Horace Mann</cite></p>'
 
-quotes[18]='<p>Let us not be content to wait and see what will happen, but give us the determination to make the right things happen. <i>-Peter Marshall</i>'
+quotes[18]='<p>Let us not be content to wait and see what will happen, but give us the determination to make the right things happen.<p> <p><cite>-Peter Marshall</cite></p>'
 
-quotes[19]='<p>I hear and I forget, I see and I remember. I do and I understand. <i>-Chinese Proverb</i>'
+quotes[19]='<p>I hear and I forget, I see and I remember. I do and I understand.<p> <p><cite>-Chinese Proverb</cite></p>'
 
-quotes[20]='<p>One may walk over the highest mountain one step at a time. <i>-John Wanamaker</i>'
+quotes[20]='<p>One may walk over the highest mountain one step at a time.<p> <p><cite>-John Wanamaker</cite></p>'
 
-quotes[21]='<p>Every action is either strong or weak, and when every action is strong we are successful. <i>-Wallace D. Wattles</i>'
+quotes[21]='<p>Every action is either strong or weak, and when every action is strong we are successful.<p> <p><cite>-Wallace D. Wattles</cite></p>'
 
-quotes[22]='<p>If we do what is necessary, all the odds are in our favor. <i>-Henry Kissinger</i>'
+quotes[22]='<p>If we do what is necessary, all the odds are in our favor.<p> <p><cite>-Henry Kissinger</cite></p>'
 
-quotes[23]='<p>Little minds are tamed and subdued by misfortune; but great minds rise above them. <i>-Washington Irving</i>'
+quotes[23]='<p>Little minds are tamed and subdued by misfortune; but great minds rise above them.<p> <p><cite>-Washington Irving</cite></p>'
 
-quotes[24]='<p>When an affliction happens to you, you either let it defeat you, or you defeat it... <i>-Rosalind Russell</i>'
+quotes[24]='<p>When an affliction happens to you, you either let it defeat you, or you defeat it.<p> <p><cite>-Rosalind Russell</cite></p>'
 
-quotes[25]='<p>There\'s a basic human weakness inherent in all people which tempts them to want what they can\'t have and not want what is readily available to them. <i>-Robert J. Ringer</i>'
+quotes[25]='<p>There\'s a basic human weakness inherent in all people which tempts them to want what they can\'t have and not want what is readily available to them.<p> <p><cite>-Robert J. Ringer</cite></p>'
 
-quotes[26]='<p>If there is something to gain and nothing to lose by asking, by all means ask! <i>-W. Clement Stone</i>'
+quotes[26]='<p>If there is something to gain and nothing to lose by asking, by all means ask.<p> <p><cite>-W. Clement Stone</cite></p>'
 
-quotes[27]='<p>It\'s not the situation ... It\'s your reaction to the situation <i>-Robert Conklin</i>'
+quotes[27]='<p>It\'s not the situation ... It\'s your reaction to the situation.<p> <p><cite>-Robert Conklin</cite></p>'
 
-quotes[28]='<p>Life at any time can become difficult: life at any time can become easy.  It all depends upon how one adjusts oneself to life. <i>-Morarji Desai</i>'
+quotes[28]='<p>Life at any time can become difficult: life at any time can become easy.  It all depends upon how one adjusts oneself to life.<p> <p><cite>-Morarji Desai</cite></p>'
 
-quotes[29]='<p>What happens is not as important as how you react to what happens. <i>-Thaddeus Golas</i>'
+quotes[29]='<p>What happens is not as important as how you react to what happens.<p> <p><cite>-Thaddeus Golas</cite></p>'
 
-quotes[30]='<p>To hell with circumstances; I create opportunities. <i>-Bruce Lee</i>'
+quotes[30]='<p>To hell with circumstances; I create opportunities.<p> <p><cite>-Bruce Lee</cite></p>'
 
 var whichquote=Math.floor(Math.random()*(quotes.length))
 document.write(quotes[whichquote])
