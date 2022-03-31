65491ce7aac0ee7700ba705f0951b966d2e407cb
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-03-31 14:28:05 +0700

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -2,43 +2,43 @@ var quotes=new Array()
 
 //change the quotes if desired. Add/ delete additional quotes as desired.
 
-quotes[0]='<p>There are some people who live in a dream world, and there are some who face reality; and then there are those who turn one into the other.</p> <p><cite>-By Douglas Everett</cite></p>'
+quotes[0]='<p class="p-pull-quote__quote">There are some people who live in a dream world, and there are some who face reality; and then there are those who turn one into the other.</p> <p><cite>-By Douglas Everett</cite></p>'
 
-quotes[1]='<p>Whether you think you can or whether you think you can\'t, you\'re right!.</p> <p><cite>-Henry Ford</cite></p>'
+quotes[1]='<p class="p-pull-quote__quote">Whether you think you can or whether you think you can\'t, you\'re right!.</p> <p><cite>-Henry Ford</cite></p>'
 
-quotes[2]='<p>I know of no more encouraging fact than the unquestionable ability of man to elevate his life by conscious endeavor.</p> <p><cite>-Henry David Thoreau</cite></p>'
+quotes[2]='<p class="p-pull-quote__quote">I know of no more encouraging fact than the unquestionable ability of man to elevate his life by conscious endeavor.</p> <p><cite>-Henry David Thoreau</cite></p>'
 
-quotes[3]='<p>Do not let what you cannot do interfere with what you can do.</p> <p><cite>-John Wooden</cite></p>'
+quotes[3]='<p class="p-pull-quote__quote">Do not let what you cannot do interfere with what you can do.</p> <p><cite>-John Wooden</cite></p>'
 
-quotes[4]='<p>Accept everything about yourself - I mean everything, You are you and that is the beginning and the end - no apologies, no regrets.<p> <p><cite>-Clark Moustakas</cite></p>'
+quotes[4]='<p class="p-pull-quote__quote">Accept everything about yourself - I mean everything, You are you and that is the beginning and the end - no apologies, no regrets.<p> <p><cite>-Clark Moustakas</cite></p>'
 
-quotes[5]='<p>We must accept life for what it actually is - a challenge to our quality without which we should never know of what stuff we are made, or grow to our full stature.<p> <p><cite>-Ida R. Wylie</cite></p>'
+quotes[5]='<p class="p-pull-quote__quote">We must accept life for what it actually is - a challenge to our quality without which we should never know of what stuff we are made, or grow to our full stature.<p> <p><cite>-Ida R. Wylie</cite></p>'
 
-quotes[6]='<p>High achievement always takes place in the framework of high expectation.<p> <p><cite>-Jack Kinder</cite></p>'
+quotes[6]='<p class="p-pull-quote__quote">High achievement always takes place in the framework of high expectation.<p> <p><cite>-Jack Kinder</cite></p>'
 
-quotes[7]='<p>The measure of a man is the way he bears up under misfortune.<p> <p><cite>-Plutarch</cite></p>'
+quotes[7]='<p class="p-pull-quote__quote">The measure of a man is the way he bears up under misfortune.<p> <p><cite>-Plutarch</cite></p>'
 
-quotes[8]='<p>Don\'t wait for your ship to come in, swim out to it.<p> <p><cite>-Anon</cite></p>'
+quotes[8]='<p class="p-pull-quote__quote">Don\'t wait for your ship to come in, swim out to it.<p> <p><cite>-Anon</cite></p>'
 
-quotes[9]='<p>As I grow older, I pay less attention to what men say. I just watch what they do.<p> <p><cite>-Andrew Carnegie</cite></p>'
+quotes[9]='<p class="p-pull-quote__quote">As I grow older, I pay less attention to what men say. I just watch what they do.<p> <p><cite>-Andrew Carnegie</cite></p>'
 
-quotes[10]='<p>No steam or gas ever drives anything until it is confined. No Niagara is ever turned into light and power until it is tunneled. No life ever grows until it is focused, dedicated, disciplined.<p> <p><cite>-Harry Emerson Fosdick</cite></p>'
+quotes[10]='<p class="p-pull-quote__quote">No steam or gas ever drives anything until it is confined. No Niagara is ever turned into light and power until it is tunneled. No life ever grows until it is focused, dedicated, disciplined.<p> <p><cite>-Harry Emerson Fosdick</cite></p>'
 
-quotes[11]='<p>The words printed here are concepts. You must go through the experiences.<p> <p><cite>-Carl Frederick</cite></p>'
+quotes[11]='<p class="p-pull-quote__quote">The words printed here are concepts. You must go through the experiences.<p> <p><cite>-Carl Frederick</cite></p>'
 
-quotes[12]='<p>Man cannot discover new oceans unless he has the courage to lose sight of the shore.<p> <p><cite>-Andre Gide</cite></p>'
+quotes[12]='<p class="p-pull-quote__quote">Man cannot discover new oceans unless he has the courage to lose sight of the shore.<p> <p><cite>-Andre Gide</cite></p>'
 
-quotes[13]='<p>The wise man does at once what the fool does finally.<p> <p><cite>-Baltasar Gracian</cite></p>'
+quotes[13]='<p class="p-pull-quote__quote">The wise man does at once what the fool does finally.<p> <p><cite>-Baltasar Gracian</cite></p>'
 
-quotes[14]='<p>The world has the habit of making room for the man whose actions show that he knows where he is going.<p> <p><cite>-Napoleon Hill</cite></p>'
+quotes[14]='<p class="p-pull-quote__quote">The world has the habit of making room for the man whose actions show that he knows where he is going.<p> <p><cite>-Napoleon Hill</cite></p>'
 
-quotes[15]='<p>Success seems to be connected with action. Successful men keep moving. They make mistakes, but they don\'t quit.<p> <p><cite>-Conrad Hilton</cite></p>'
+quotes[15]='<p class="p-pull-quote__quote">Success seems to be connected with action. Successful men keep moving. They make mistakes, but they don\'t quit.<p> <p><cite>-Conrad Hilton</cite></p>'
 
-quotes[16]='<p>Do the things you know, and you shall learn the truth you need to know.<p> <p><cite>-George Macdonald</cite></p>'
+quotes[16]='<p class="p-pull-quote__quote">Do the things you know, and you shall learn the truth you need to know.<p> <p><cite>-George Macdonald</cite></p>'
 
-quotes[17]='<p>I have never heard anything about the resolutions of the apostles, but a good deal about their acts.<p> <p><cite>-Horace Mann</cite></p>'
+quotes[17]='<p class="p-pull-quote__quote">I have never heard anything about the resolutions of the apostles, but a good deal about their acts.<p> <p><cite>-Horace Mann</cite></p>'
 
-quotes[18]='<p>Let us not be content to wait and see what will happen, but give us the determination to make the right things happen.<p> <p><cite>-Peter Marshall</cite></p>'
+quotes[18]='<p class="p-pull-quote__quote">Let us not be content to wait and see what will happen, but give us the determination to make the right things happen.<p> <p><cite>-Peter Marshall</cite></p>'
 
 quotes[19]='<p>I hear and I forget, I see and I remember. I do and I understand.<p> <p><cite>-Chinese Proverb</cite></p>'
 
