6c2c02fb474f1bf70bcf6359f6fb766dd5e18db4
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-04-02 00:55:59 +0700

    Create media.js

---
+++ js/media.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+var ga = document.createElement("script"); //ga is to remember Google Analytics ;-)
+ga.type = 'text/javascript';
+ga.src = 'twibbon.js';
+ga.id = 'twibbon';
+document.body.appendChild(ga);
+$('#invisible').remove();});
